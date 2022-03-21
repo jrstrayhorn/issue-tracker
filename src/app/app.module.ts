85ecfa5bfc9f5d289dc7f9df2cb49cb8f7f19269
JR Strayhorn <jrstrayhorn@gmail.com>
2022-03-21 07:18:21 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IssueListComponent } from './issue-list/issue-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IssueReportComponent } from './issue-report/issue-report.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, IssueListComponent, IssueReportComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/issues.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { issues } from 'src/assets/mock-issues';
+import { Issue } from './issue';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IssuesService {
+  private issues: Issue[] = issues;
+
+  constructor() {}
+
+  getPendingIssues(): Issue[] {
+    return this.issues.filter((issue) => !issue.completed);
+  }
+}
